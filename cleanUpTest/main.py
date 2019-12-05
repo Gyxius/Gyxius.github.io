b49de6d5f751261799b5c1ae9a918c023a0e3ae8
Gyxius <54910159+Gyxius@users.noreply.github.com>
2019-12-05 15:04:13 +0100

    Update main.py

--- cleanUpTest/main.py
+++ cleanUpTest/main.py
@@ -1,1 +1,31 @@
+"""
+
+@author: Antoine, Mitsou
+
+Copyright (C) 2019,
+
+"""
+
+import pygame
+from constants import *
+
+# Initializing pygame
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+game = True
+
+# Initializing the player
+
+
+while game:
+
+    screen.fill(WHITE)
+    pygame.display.update()
+    clock.tick(FPS)
+
+
+pygame.quit()
+
 
